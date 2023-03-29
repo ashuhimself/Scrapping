be5460cc542ec1b73d923e4593116bf9bdb39a4a
Ashutosh <ashuhimself@outlook.com>
2023-03-30 03:32:04 +0530

    Assignment

---
+++ ReadMe.md
@@ -0,0 +1,43 @@
+#Cermati Assignment
+---
+This is a Python script that scrapes job postings from Cermati website and saves them as a JSON file.
+
+Installation
+To run this script, you need to have Python 3 and the following libraries installed:
+
+requests
+
+You can install them using pip:
+
+```Python
+pip install requests
+```
+Usage
+To use this script, you need to clone this repository or download the main.py file. Then, you can run the script from the command line:
+
+```Python
+python main.py
+```
+The script will create two files in the same directory: index.html and data.json. The index.html file contains the HTML source code of the Cermati website. The data.json file contains the scraped job postings organized by department.
+
+The data.json file has the following structure as asked in assignement:
+```pyhon
+{
+    "department": [
+        {
+            "title": "job title",
+            "location": "job location",
+            "description": "job description",
+            "qualification": "job qualification",
+            "posted by": "job poster"
+        },
+        ...
+    ],
+    ...
+}
+```
+
+
+
+#Contact
+If you have any questions or feedback, please contact me You have my details.
---
+++ main.py
@@ -0,0 +1,51 @@
+import requests
+import time
+import re
+import json
+import logging
+import itertools
+
+
+start_time = time.time()
+json_data = []
+url = 'https://www.cermati.com/karir'
+logging.basicConfig(filename='log.txt', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+
+with requests.Session() as session:
+    html = session.get(url).text
+    with open("index.html", "w") as f:
+        f.write(html)
+    pattern = re.compile(r'\bref":"(https?://[^"]+)')
+    job_urls = pattern.findall(html)
+    for job_url in job_urls:
+        try:
+            json_response = session.get(job_url)
+            json_response.raise_for_status()
+            data = json_response.json()
+            final = {
+                "department": data['department']['label'],
+                "title": data['name'],
+                "location": data['location']['region'] + ', ' + data['customField'][2]["valueLabel"],
+                "description": re.sub('<[^<]+?>', '', data['jobAd']['sections']['jobDescription']['text']).replace('&#xa0;', ''),
+                "qualification": re.sub('<[^<]+?>', '', data['jobAd']['sections']['qualifications']['text']).replace('&#xa0;', ''),
+                "posted by": data['creator']['name']
+                } 
+            json_data.append(final)
+        except Exception as e:
+            logging.exception(f"Error occurred while parsing data from {job_url}")
+
+data = sorted(json_data, key=lambda x: x["department"])
+
+grouped_data = {}
+for department, group in itertools.groupby(data, key=lambda x: x["department"]):
+    job_postings = [job_posting.copy() for job_posting in group]
+    for job_posting in job_postings:
+        del job_posting["department"]
+    grouped_data[department] = job_postings
+
+with open('data.json', 'w') as outfile:
+    json.dump(grouped_data, outfile, indent=4)
+
+end_time = time.time()
+runtime = end_time - start_time
+print(f"Program runtime: {runtime:.2f} seconds")
